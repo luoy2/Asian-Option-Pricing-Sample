42ff8b66c1a260e3a27a797e0989980b1525d02f
Yikang Luo <luoy2@beloit.edu>
2016-04-17 22:17:48 -0500

    Initial Commit

---
+++ Asian_Option/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Asian_Option
+//
+//  Created by Yikang Luo on 16/4/17.
+//  Copyright © 2016年 Yikang Luo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
